8616f1f3fcffce81a645f6955f3a7fe4a2de347a
Alexander Chuzhoy <achuzhoy@redhat.com>
2017-10-23 14:17:23 -0400

    attempting to write somethign to mysql
    Signed-off-by: Alexander Chuzhoy <achuzhoy@redhat.com>

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
+import MySQLdb
+
 def main():
-    print("Hello world")
-    input() #to wait 
+   db = MySQLdb.connect(host="10.130.34.252",user="root",passwd="redhat",db="vacation")
+   cursor = db.cursor()
+   cursor.execute("insert into rooms (familyname,roomname) values ('a1','b1');)
+   db.commit()
+   db.close()
+
+
 if __name__ == '__main__':
     main()
